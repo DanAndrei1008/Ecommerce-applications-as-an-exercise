83adbad1e69039888c08af529b2e7279a85f8e2c
Dan Dosius <92085399+DanAndrei1008@users.noreply.github.com>
2024-02-21 23:09:48 +0200

    Add files via upload

---
+++ Data/Suppliers/Supplier.cs
@@ -0,0 +1,11 @@
+﻿namespace Internship.Data
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+        public bool Active { get; set; }
+    }
+}
---
+++ Data/Suppliers/SupplierConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Internship.Data
+{
+
+    public class SupplierConfiguration : IEntityTypeConfiguration<Supplier>
+    {
+        public void Configure(EntityTypeBuilder<Supplier> builder)
+        {
+           builder.ToTable(name:nameof(Supplier));
+           builder.HasKey(x => x.Id);
+           builder.Property(x => x.Name).IsRequired();
+           builder.Property(x=>x.Location).IsRequired();
+           builder.Property(x=>x.Active).IsRequired();
+        }
+    }
+}
