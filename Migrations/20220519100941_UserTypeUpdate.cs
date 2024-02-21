9e8553b1024ed0f935664a276d6517be2aa2f96d
Dan Dosius <92085399+DanAndrei1008@users.noreply.github.com>
2024-02-21 23:11:11 +0200

    Add files via upload

---
+++ Migrations/20220519100941_UserTypeUpdate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Internship.Migrations
+{
+    public partial class UserTypeUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AttributeTypeId",
+                table: "Attribute",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AttributeTypeId",
+                table: "Attribute",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
---
+++ Migrations/20220519101525_UserType.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Internship.Migrations
+{
+    public partial class UserType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "UserType",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Physical Person" });
+
+            migrationBuilder.InsertData(
+                table: "UserType",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Legal Person" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "UserType",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "UserType",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
